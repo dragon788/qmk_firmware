a17dc3fc4a6fa203cc13099875630cf76bed3cf5
dragon788 <dragon788@users.noreply.github.com>
2016-04-09 13:57:24 -0500

    Better arrows
    Sticking with Vim order for arrow keys as well, separates left/right up/down across both hands for hopefully easier navigation and alternation.

--- keyboard/ergodox_ez/keymaps/dragon788/keymap.c
+++ keyboard/ergodox_ez/keymaps/dragon788/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ~L1  | Grv  |  '"  | Left |Rgt/L2|                                       | Up/L2| Down |   [  |   ]  | ~L1  |
+ *   | ~L1  | Grv  |  '"  | Left |Up/L2 |                                       |Rgt/L2| Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | Home |       | PgUp |Ctrl/Esc|
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(1),
         CTL_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-         KC_FN1,      KC_GRV,      KC_QUOT,KC_LEFT, LT(MDIA, KC_RGHT),
+         KC_FN1,      KC_GRV,      KC_QUOT,KC_LEFT, LT(MDIA, KC_DOWN),
                                                      KC_APP,  KC_HOME,
                                                                KC_END,
                                               KC_BSPC,KC_DELT,ALT_T(INS),
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(2),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,           KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, CTL_T(KC_QUOT),
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,        KC_RSFT,
-                       LT(MDIA, KC_UP),   KC_DOWN,KC_LBRC,KC_RBRC,         KC_FN1,
+                       LT(MDIA, KC_UP),   KC_RGHT,KC_LBRC,KC_RBRC,         KC_FN1,
              KC_PGUP,        CTL_T(KC_ESC),
              KC_PGDN,
              ALT_T(KC_TAB),KC_ENT, KC_SPC
